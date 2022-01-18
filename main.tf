83175a9c857bf23fa6ab861078bc66048945b25a
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-18 19:31:49 +0530

    with sleep

--- main.tf
+++ main.tf
@@ -130,6 +130,15 @@ resource "vsphere_virtual_machine" "vm-db" {
 
 }
 
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_180_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "180s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
 resource "null_resource" "next" {
   depends_on = [time_sleep.wait_180_seconds]
 }
