4cf3f4e21198d4f661109cfae9734cc99e54035e
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-18 14:39:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-
+#connecting to the Linux OS having the Ansible playbook
+resource "null_resource" "nullremote2" {
+depends_on = [aws_volume_attachment.ebs_att]  
+connection {
+	type     = "ssh"
+	user     = "root"
+	password = "${var.ansible_password}"
+    	host= "${var.ansible_host}"
+}
+#command to run ansible playbook on remote Linux OS
+provisioner "remote-exec" {
+    
+    inline = [
+	"cd /root/ansible_terraform/",
+	"ansible-playbook instance.yml"
+]
+}
+}
