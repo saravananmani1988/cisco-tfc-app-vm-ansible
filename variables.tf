f939c6ed100c8e75142ff1be974526e5ebcc827a
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-18 14:41:18 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "ansible_host" {
+  type        = string
+  description = "Ansible server IP or host name"
+}
+variable "ansible_password" {
+  type        = string
+  description = "Ansible Server root password"
+}
